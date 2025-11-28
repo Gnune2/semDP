2ed6fe2803bf5f410908d2809eafa62f4fb02e61
Gnune2 <226509660+Gnune2@users.noreply.github.com>
2025-11-28 03:44:58 -0300

    BIG UPDATE AND FEAT: melhora a adição das matérias para a página príncipal transformando em cards pelo script (matérias.js) usando bootstrap, facilitando a adição de novas matérias, e adiciona imagens para cada matéria.

--- back-end/materias.js
+++ back-end/materias.js
@@ -2,41 +2,73 @@ const materias = [
     {
         id: 1,
         nome: "Matemática Discreta",
-        Imagem:"Matemática Discreta",
+        imagem:"/assets/materias/matematica-discreta.png",
     },
     {
         id: 2,
         nome: "Estatística",
-        Imagem:"Matemática Discreta",
+        imagem:"/assets/materias/estatistica.png",
     },
     {
         id: 3,
         nome: "Desenvolvimento Front-End",
-        Imagem:"Matemática Discreta",
+        imagem:"/assets/materias/desenvolvimentoFrontEnd.png",
     },
     {
         id: 4,
-        nome: "Interface e experiência do usuário",
-        Imagem:"Matemática Discreta",
+        nome: "UX",
+        imagem:"/assets/materias/interfaceEExperienciaDoUsuario.png",
     },
     {
         id: 5,
-        nome:  "Legislação e Ética",
-        Imagem:"Matemática Discreta",
+        nome: "Legislação e Ética",
+        imagem:"/assets/materias/legislacaoEEtica.png",
     }
 ];
 
 
 function divMaterias(materias) {
-    const div = document.getElementById('main-index');
+    
     for (const materia of materias) {
-        const divMateria = document.createElement("div");
-        divMateria.id = `materia-${materia.id}`
-        divMateria.className = "materias"
-        divMateria.textContent = `${materia.nome}`
-        if (div){
-            div.appendChild(divMateria);
-            console.log("A div foi anexada com sucesso!");
+        // cria o card para receber a materia e da atributos a ela
+        const card = document.getElementById('materias');
+        const CardMateria = document.createElement("div");
+        CardMateria.id = `materia-${materia.id}`;
+        CardMateria.className = "materias col-2 p-0 card h-100 d-flex flex-column";
+        // envia o card para o documento html
+        if (card){
+            card.appendChild(CardMateria);
+        }
+        // recebe a imagem
+        const cardImagem = document.getElementById(`materia-${materia.id}`);
+        const imagem = document.createElement("img");
+        imagem.src = `${materia.imagem}`;
+        // manda imagem para o card no documento html
+        if (cardImagem) {
+            cardImagem.appendChild(imagem);
+        }
+        // cria a div corpo do card para o documento html
+        const bodyCard = document.getElementById(`materia-${materia.id}`);
+        const CardBodyMateria = document.createElement("div");
+        CardBodyMateria.id = `body-${materia.id}`;
+        CardBodyMateria.className = "card-body p-2 d-flex flex-column flex-grow-1 ";
+        if (bodyCard) {
+            bodyCard.appendChild(CardBodyMateria)
+        }
+        //nome da matéria
+        const nome = document.getElementById(CardBodyMateria.id);
+        const nomeMateria = document.createElement("h5");
+        if(nome){
+            nome.appendChild(nomeMateria)
+        }
+        nomeMateria.textContent = `${materia.nome}`;
+        //botão que redireciona para a página da matéria escolhida
+        const botao = document.getElementById(CardBodyMateria.id);
+        const botaoLink = document.createElement("a");
+        botaoLink.textContent = "Calcular Média"
+        botaoLink.className = "btn btn-outline-primary btn-sm mt-auto"
+        if (botao){
+            botao.appendChild(botaoLink);
         }
     }   
 }
