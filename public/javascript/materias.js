73421e3c6c46b8c9cfe19f69e933153a5d9d964a
Gnune2 <226509660+Gnune2@users.noreply.github.com>
2025-12-13 03:08:04 -0300

    feat: adiciona servidor no render e substitui o localhost pelo link do render.

--- public/javascript/materias.js
+++ public/javascript/materias.js
@@ -3,7 +3,7 @@ async function carregarMaterias() {
     try {
         // Agora usamos o fetchProtected direto!
         // Não precisa passar headers, ele resolve sozinho.
-        const response = await window.fetchProtected('http://localhost:3000/materia');
+        const response = await window.fetchProtected('https://media-imt.onrender.com/cadastro/materia');
 
         if (!response.ok) {
             throw new Error('Falha ao buscar matérias');
